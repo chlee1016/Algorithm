efd48800aef75cb3cbc591a643aedba2920cd36e
ckdgus1433 <ckdgus1433@gmail.com>
2020-07-11 14:58:48 +0900

    solved problem

---
+++ src/math/2775.py
@@ -0,0 +1,19 @@
+T = int(input())
+for _ in range(T):
+    k = int(input())
+    n = int(input())
+
+    board = [[0]*n for _ in range(k+1)]
+
+    for j in range(k+1):
+        for i in range(n):
+            if j == 0:
+                board[j][i] = i+1
+            elif i == 0:
+                board[j][i] = 1
+            else:
+                board[j][i] = board[j][i-1] + board[j-1][i]
+    print(board[k][n-1])
+    # for row in board:
+    #     print(*row)
+    # print()
