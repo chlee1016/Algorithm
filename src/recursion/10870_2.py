b736f0d626344e2ddfc468c72563e1644ac5447a
ckdgus1433 <ckdgus1433@gmail.com>
2020-07-12 10:40:47 +0900

    solved problem

---
+++ src/recursion/10870_2.py
@@ -0,0 +1,15 @@
+import sys
+sys.setrecursionlimit(50000)
+n = int(input())
+
+
+def Fibo(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+
+    return Fibo(n-2) + Fibo(n-1)
+
+
+print(Fibo(n))
