ef1f951432421d8d0eb981aaf819c0f459ea135c
ckdgus1433 <ckdgus1433@gmail.com>
2020-07-12 10:32:29 +0900

    solved problem

---
+++ src/recursion/10872_2.py
@@ -0,0 +1,17 @@
+import sys
+sys.setrecursionlimit(50000)
+
+
+
+def factorial(N):
+    if N == 0:
+        return 1
+    if N == 1:
+        return 1
+    return N * factorial(N-1)
+
+
+N = int(input())
+print(factorial(N))
+
+
